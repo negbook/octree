12f0334ee866ccb2f0e24d4bc0c8553998ca04de
negbook <negbook@gmail.com>
2022-04-02 21:17:33 +0800

    crazy optimise
    Update octree.lua

--- octree.lua
+++ octree.lua
@@ -1,3 +1,15 @@
+local setmetatable = setmetatable
+local DrawLine = DrawLine
+local GetEntityCoords = GetEntityCoords
+local Wait = Wait
+local CreateThread = CreateThread
+local table = table
+local ipairs = ipairs
+local math = math
+local GetPlayerPed = GetPlayerPed
+local GetEntityCoords = GetEntityCoords
+local vector3 = vector3
+local vector2 = vector2
 OcTree = {}
 local Contains = {
     pointtopoint = function(pointA,pointB,radius)
@@ -6,11 +18,37 @@ local Contains = {
     end,
     pointtobox = function(pointA,box,radius)
         local radius = radius or 0
-        return pointA.x >= box.center.x - box.size.x/2 - radius and pointA.x <= box.center.x + box.size.x/2 + radius and pointA.y >= box.center.y - box.size.y/2 - radius and pointA.y <= box.center.y + box.size.y/2 + radius and pointA.z >= box.center.z - box.size.z/2 - radius and pointA.z <= box.center.z + box.size.z/2 + radius
+        --return pointA.x >= box.center.x - box.size.x/2 - radius and pointA.x <= box.center.x + box.size.x/2 + radius and pointA.y >= box.center.y - box.size.y/2 - radius and pointA.y <= box.center.y + box.size.y/2 + radius and pointA.z >= box.center.z - box.size.z/2 - radius and pointA.z <= box.center.z + box.size.z/2 + radius
+        local pointAx = pointA.x
+        local pointAy = pointA.y
+        local pointAz = pointA.z
+        local boxCenterx = box.center.x
+        local boxCentery = box.center.y
+        local boxCenterz = box.center.z
+        local boxSizeX = box.size.x
+        local boxSizeY = box.size.y
+        local boxSizeZ = box.size.z
+
+        return pointAx >= boxCenterx - boxSizeX/2 - radius and pointAx <= boxCenterx + boxSizeX/2 + radius and pointAy >= boxCentery - boxSizeY/2 - radius and pointAy <= boxCentery + boxSizeY/2 + radius and pointAz >= boxCenterz - boxSizeZ/2 - radius and pointAz <= boxCenterz + boxSizeZ/2 + radius
     end,
     boxtobox = function(boxA,boxB,radius)
         local radius = radius or 0
-        return boxA.center.x - boxA.size.x/2 - radius <=  boxB.center.x + boxB.size.x/2 + radius and boxA.center.y - boxA.size.y/2 - radius <=  boxB.center.y + boxB.size.y/2 + radius and boxA.center.z - boxA.size.z/2 - radius <=  boxB.center.z + boxB.size.z/2 + radius
+        --return boxA.center.x - boxA.size.x/2 - radius <=  boxB.center.x + boxB.size.x/2 + radius and boxA.center.y - boxA.size.y/2 - radius <=  boxB.center.y + boxB.size.y/2 + radius and boxA.center.z - boxA.size.z/2 - radius <=  boxB.center.z + boxB.size.z/2 + radius
+        local boxACenterx = boxA.center.x
+        local boxACentery = boxA.center.y
+        local boxACenterz = boxA.center.z
+        local boxASizeX = boxA.size.x
+        local boxASizeY = boxA.size.y
+        local boxASizeZ = boxA.size.z
+
+        local boxBCenterx = boxB.center.x
+        local boxBCentery = boxB.center.y
+        local boxBCenterz = boxB.center.z
+        local boxBSizeX = boxB.size.x
+        local boxBSizeY = boxB.size.y
+        local boxBSizeZ = boxB.size.z
+
+        return boxACenterx - boxASizeX/2 - radius <=  boxBCenterx + boxBSizeX/2 + radius and boxACentery - boxASizeY/2 - radius <=  boxBCentery + boxBSizeY/2 + radius and boxACenterz - boxASizeZ/2 - radius <=  boxBCenterz + boxBSizeZ/2 + radius
     end,
 }
 
@@ -60,73 +98,92 @@ function OcTree:inner_subdivide()
     local childwidth = parentwidth / 2
     local childlength = parentlength / 2
     local childhight = parenthight / 2
+    local toleftX = parentcenter.x - childwidth/2
+    local toupY = parentcenter.y - childlength/2
+    local tofloorZ = parentcenter.z - childhight/2
     
+    local torightX = parentcenter.x + childwidth/2
+    local todownY = parentcenter.y + childlength/2
+    local toroofZ = parentcenter.z + childhight/2
+
+    local childlefttopcenter_topbox = vector3(parentcenter.x - childwidth/2 , parentcenter.y + childlength/2, parentcenter.z + childhight/2)
+    local childrighttopcenter_topbox = vector3(parentcenter.x + childwidth/2 , parentcenter.y + childlength/2, parentcenter.z + childhight/2)
+    local childleftbottomcenter_topbox = vector3(parentcenter.x - childwidth/2 , parentcenter.y - childlength/2, parentcenter.z + childhight/2)
+    local childrightbottomcenter_topbox = vector3(parentcenter.x + childwidth/2 , parentcenter.y - childlength/2, parentcenter.z + childhight/2)
+
+    local childlefttopcenter_bottombox = vector3(parentcenter.x - childwidth/2 , parentcenter.y + childlength/2, parentcenter.z - childhight/2)
+    local childrighttopcenter_bottombox = vector3(parentcenter.x + childwidth/2 , parentcenter.y + childlength/2, parentcenter.z - childhight/2)
+    local childleftbottomcenter_bottombox = vector3(parentcenter.x - childwidth/2 , parentcenter.y - childlength/2, parentcenter.z - childhight/2)
+    local childrightbottomcenter_bottombox = vector3(parentcenter.x + childwidth/2 , parentcenter.y - childlength/2, parentcenter.z - childhight/2)
+
+    local childsize = vector3(childwidth,childlength,childhight)
     --create new quadtrees in 8 box regions
     self.topbox_lefttop = OcTree.new(
         {
-            center = vector3(parentcenter.x - childwidth/2 , parentcenter.y + childlength/2, parentcenter.z + childhight/2),
-            game_center = vector3(parentcenter.x - childwidth/2 , parentcenter.y + childlength/2, parentcenter.z - childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childlefttopcenter_topbox,
+            game_center = childlefttopcenter_topbox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
     self.topbox_righttop = OcTree.new(
         {
-            center = vector3(parentcenter.x + childwidth/2 , parentcenter.y + childlength/2, parentcenter.z + childhight/2),
-            game_center = vector3(parentcenter.x + childwidth/2 , parentcenter.y + childlength/2, parentcenter.z - childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childrighttopcenter_topbox,
+            game_center = childrighttopcenter_topbox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
     self.topbox_leftbottom = OcTree.new(
         {
-            center = vector3(parentcenter.x - childwidth/2 , parentcenter.y - childlength/2, parentcenter.z + childhight/2),
-            game_center = vector3(parentcenter.x - childwidth/2 , parentcenter.y - childlength/2, parentcenter.z - childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childleftbottomcenter_topbox,
+            game_center = childleftbottomcenter_topbox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
     self.topbox_rightbottom = OcTree.new(
         {
-            center = vector3(parentcenter.x + childwidth/2 , parentcenter.y - childlength/2, parentcenter.z + childhight/2),
-            game_center = vector3(parentcenter.x + childwidth/2 , parentcenter.y - childlength/2, parentcenter.z - childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childrightbottomcenter_topbox,
+            game_center = childrightbottomcenter_topbox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
     self.bottombox_lefttop = OcTree.new(
         {
-            center = vector3(parentcenter.x - childwidth/2 , parentcenter.y + childlength/2, parentcenter.z - childhight/2),
-            game_center = vector3(parentcenter.x - childwidth/2 , parentcenter.y + childlength/2, parentcenter.z + childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childlefttopcenter_bottombox,
+            game_center = childlefttopcenter_bottombox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
     self.bottombox_righttop = OcTree.new(
         {
-            center = vector3(parentcenter.x + childwidth/2 , parentcenter.y + childlength/2, parentcenter.z - childhight/2),
-            game_center = vector3(parentcenter.x + childwidth/2 , parentcenter.y + childlength/2, parentcenter.z + childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childrighttopcenter_bottombox,
+            game_center = childrighttopcenter_bottombox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
     self.bottombox_leftbottom = OcTree.new(
         {
-            center = vector3(parentcenter.x - childwidth/2 , parentcenter.y - childlength/2, parentcenter.z - childhight/2),
-            game_center = vector3(parentcenter.x - childwidth/2 , parentcenter.y - childlength/2, parentcenter.z + childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childleftbottomcenter_bottombox,
+            game_center = childleftbottomcenter_bottombox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
     self.bottombox_rightbottom = OcTree.new(
         {
-            center = vector3(parentcenter.x + childwidth/2 , parentcenter.y - childlength/2, parentcenter.z - childhight/2),
-            game_center = vector3(parentcenter.x + childwidth/2 , parentcenter.y - childlength/2, parentcenter.z + childhight/2),
-            size = vector3(childwidth, childlength, childhight)
+            center = childrightbottomcenter_bottombox,
+            game_center = childrightbottomcenter_bottombox - vector3(0.0,0.0,childhight),
+            size = childsize
         },
         self.capacity
     )
-
+    
+    
     self.isdivided = true
 end
 
@@ -139,20 +196,56 @@ function OcTree:inner_intersects(box)
     local width = self.size.x
     local length = self.size.y
     local hight = self.size.z
-
+    --[[
     return (box_center.x + box_width/2 > center.x - width/2 and
             box_center.x - box_width/2 < center.x + width/2 and
             box_center.y + box_length/2 > center.y - length/2 and
             box_center.y - box_length/2 < center.y + length/2 and
             box_center.z + box_hight/2 > center.z - hight/2 and
             box_center.z - box_hight/2 < center.z + hight/2)
+    --]]
+    local boxcenterx = box_center.x
+    local boxcenterz = box_center.z
+    local boxcentery = box_center.y
+    local boxhalfwidth = box_width/2
+    local boxhalflength = box_length/2
+    local boxhalfhight = box_hight/2
+    local selfcenterx = center.x
+    local selfcenterz = center.z
+    local selfcentery = center.y
+    local selfhalfwidth = width/2
+    local selfhalflength = length/2
+    local selfhalfhight = hight/2
+
+    return (boxcenterx + boxhalfwidth > selfcenterx - selfhalfwidth and
+            boxcenterx - boxhalfwidth < selfcenterx + selfhalfwidth and
+            boxcenterz + boxhalflength > selfcenterz - selfhalflength and
+            boxcenterz - boxhalflength < selfcenterz + selfhalflength and
+            boxcentery + boxhalfhight > selfcentery - selfhalfhight and
+            boxcentery - boxhalfhight < selfcentery + selfhalfhight)
 end
 
 function OcTree:inner_point_contains (point, radius)
     local radius = radius or 0.0
+    --[[
     return point.x >= self.center.x - self.size.x/2 - radius and point.x <= self.center.x + self.size.x/2 + radius and
            point.y >= self.center.y - self.size.y/2 - radius and point.y <= self.center.y + self.size.y/2 + radius and
            point.z >= self.center.z - self.size.z/2 - radius and point.z <= self.center.z + self.size.z/2 + radius
+    --]]
+    local pointx = point.x
+    local pointz = point.z
+    local pointy = point.y
+    local selfcenterx = self.center.x
+    local selfcenterz = self.center.z
+    local selfcentery = self.center.y
+    local selfhalfwidth_withradius = self.size.x/2 + radius
+    local selfhalflength_withradius = self.size.y/2 + radius
+    local selfhalfhight_withradius = self.size.z/2 + radius
+
+    return (pointx >= selfcenterx - selfhalfwidth_withradius and pointx <= selfcenterx + selfhalfwidth_withradius and
+            pointz >= selfcenterz - selfhalflength_withradius and pointz <= selfcenterz + selfhalflength_withradius and
+            pointy >= selfcentery - selfhalfhight_withradius and pointy <= selfcentery + selfhalfhight_withradius)
+
 end
 
 function OcTree:insert_point(point)
@@ -243,10 +336,30 @@ end
 function OcTree:inner_object_contains(object)
     local center = object.center 
     local size = object.size
+    --[[
     return center.x - size.x/2 >= self.center.x - self.size.x/2 and center.x + size.x/2 <= self.center.x + self.size.x/2 and
            center.y - size.y/2 >= self.center.y - self.size.y/2 and center.y + size.y/2 <= self.center.y + self.size.y/2 and
            center.z - size.z/2 >= self.center.z - self.size.z/2 and center.z + size.z/2 <= self.center.z + self.size.z/2
+    --]]
+    local selfcenterx = self.center.x
+    local selfcenterz = self.center.z
+    local selfcentery = self.center.y
+    local selfhalfwidth = self.size.x/2
+    local selfhalflength = self.size.y/2
+    local selfhalfhight = self.size.z/2
+    local objectcenterx = center.x
+    local objectcenterz = center.z
+    local objectcentery = center.y
+    local objecthalfwidth = size.x/2
+    local objecthalflength = size.y/2
+    local objecthalfhight = size.z/2
+
+    return (objectcenterx - objecthalfwidth >= selfcenterx - selfhalfwidth and objectcenterx + objecthalfwidth <= selfcenterx + selfhalfwidth and
+            objectcenterz - objecthalflength >= selfcenterz - selfhalflength and objectcenterz + objecthalflength <= selfcenterz + selfhalflength and
+            objectcentery - objecthalfhight >= selfcentery - selfhalfhight and objectcentery + objecthalfhight <= selfcentery + selfhalfhight)
+
 end
+
 
 function OcTree:insert_object(catagary_name,object)
     if not self:inner_object_contains(object) then
